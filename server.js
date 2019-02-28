94756379a4498b9c7bb7fb5f9815f6a7b100845f
Alex C Thain <45198025+alexcharlesthain@users.noreply.github.com>
2019-02-28 11:53:55 +0000

    Add files via upload
    This latest commit is the working code for the express server working within react app to upload a file, successfully creating an upload folder in which it places the file.

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const multer = require('multer');
 const upload = multer({
-  dest: 'uploads/'
-}); 
+	dest: 'uploads/'
+});
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/client/src/App.js');
 });
 
 app.post('/', upload.single('file-to-upload'), (req, res) => {
   res.redirect('/');
 });
 
-app.listen(4000);
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+// GET route
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
