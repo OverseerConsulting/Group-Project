b741d7d78971c2359bb85b370e1b62261c7ec2c1
jc1 <jc1sterling@gmail.com>
2019-03-04 14:57:27 +0000

    fully functional registration page that stores a new user into DB

--- Store-and-get-a-report/index.test.js
+++
@@ -1,8 +0,0 @@
-const routes = require('./routes/api');
-const Potter = require('../models/harrypotternames');
-
-test(' get from data base', () => {
-    expect(router.get('/potter', function(req, res) {
-    Potter.find({ name : Cho}).toBe('{"_id":"5c73ff4db651a930c4ac80fc","Name":"Cho","Score":1}');
-    }
-})));
