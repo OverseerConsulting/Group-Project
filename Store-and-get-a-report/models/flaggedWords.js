71e5e47bbe81c7cd7e7292f14f9f7e5e0b76d268
jc1 <jc1sterling@gmail.com>
2019-03-05 14:13:30 +0000

    now connects to hosted database, also added get by param endpoint

---
+++ Store-and-get-a-report/models/flaggedWords.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var flaggedWordsSchema = new Schema({
+    ListName: String,
+    Words: [{
+        Word: String,
+        Score: Number
+    }]
+})
+module.exports = mongoose.model('flaggedwords', flaggedWordsSchema);
